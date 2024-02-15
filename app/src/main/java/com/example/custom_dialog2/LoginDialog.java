f6be6fe51735735fce9d7803609473b7fffc7fef
Prasad <prasadrjadhav11@gmail.com>
2024-02-15 10:13:12 +0530

    custon dialog 2

---
+++ app/src/main/java/com/example/custom_dialog2/LoginDialog.java
@@ -0,0 +1,56 @@
+package com.example.custom_dialog2;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginDialog extends Dialog {
+    private EditText edtUsername,edtPassword;
+    private Button btnLogin;
+    private Context context;
+
+
+    interface  OnLoginListener{
+        void onSuccess(LoginDialog loginDialog);
+        void onFail(LoginDialog loginDialog);
+    }
+    private  OnLoginListener onLoginListener=null;
+
+    public  void setOnLoginListener(OnLoginListener onLoginListener){
+        this.onLoginListener=onLoginListener;
+    }
+    public  LoginDialog(Context context){
+        super(context);
+        this.context=context;
+        initViews();
+        btnLogin.setOnClickListener(new BtnLoginClickListener());
+    }
+
+    private  class BtnLoginClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view) {
+            if (edtUsername.getText().toString().equals("bitcode")&&edtPassword.getText().toString().equals("bitcode")){
+                if (onLoginListener!=null){
+                    onLoginListener.onSuccess(LoginDialog.this);
+                }else{
+                    if (onLoginListener !=null){
+                        onLoginListener.onFail(LoginDialog.this);
+                    }
+                }
+            }
+        }
+    }
+    private  void  mt(String text){
+        Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+    }
+    private  void  initViews(){
+        setContentView(R.layout.login_dialog);
+        edtPassword=findViewById(R.id.edtPassword);
+        edtUsername=findViewById(R.id.edtPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+    }
+
+}
---
+++ app/src/main/java/com/example/custom_dialog2/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.custom_dialog2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnSignIn=findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        LoginDialog loginDialog=new LoginDialog(MainActivity.this);
+                        loginDialog.setOnLoginListener(new  MyOnLoginListener());
+                        loginDialog.show();
+                    }
+                }
+        );
+    }
+
+    private  class MyOnLoginListener implements  LoginDialog.OnLoginListener{
+        @Override
+        public void onSuccess(LoginDialog loginDialog) {
+        mt("my Success action");
+        loginDialog.dismiss();
+        }
+
+        @Override
+        public void onFail(LoginDialog loginDialog) {
+            mt("My fail action");
+        }
+    }
+
+
+    private  void  mt(String text){
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+    }
+
+}
