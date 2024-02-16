bc2b0958900d5e4e93ff77eb1f0c862cdc1ef915
Prasad <prasadrjadhav11@gmail.com>
2024-02-16 12:08:45 +0530

    16-2-2024

--- app/src/main/java/com/example/custom_dialog2/MainActivity.java
+++ app/src/main/java/com/example/custom_dialog2/MainActivity.java
@@ -7,12 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 public class MainActivity extends AppCompatActivity {
 
     private Button btnSignIn;
@@ -28,9 +22,27 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         LoginDialog loginDialog = new LoginDialog(MainActivity.this);
+                        loginDialog.setOnLoginListener(new MyOnLoginListener());
                         loginDialog.show();
                     }
                 }
         );
+    }
+
+    private class MyOnLoginListener implements LoginDialog.OnLoginListener {
+        @Override
+        public void onSuccess(LoginDialog loginDialog) {
+            mt("My Success action");
+            loginDialog.dismiss();
+        }
+
+        @Override
+        public void onFail(LoginDialog loginDialog) {
+            mt("My fail action");
+        }
+    }
+
+    private void mt(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
